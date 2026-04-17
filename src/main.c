bfc1a2efc934b44b0622af9bcd0506b6469715e3
ruzicle <276037575+ruzicle@users.noreply.github.com>
2026-04-17 20:27:33 +0200

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "parser.h"
+#include "analyzer.h"
+#include "report.h"
+
+#define MAX_LINE_LEN 4096
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <logfile.txt>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char *filename = argv[1];
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error: Could not open file '%s'\n", filename);
+        return EXIT_FAILURE;
+    }
+
+    AnalyzerContext *ctx = malloc(sizeof(AnalyzerContext));
+    if (ctx == NULL) {
+        fprintf(stderr, "Error: Out of memory for AnalyzerContext.\n");
+        fclose(fp);
+        return EXIT_FAILURE;
+    }
+    analyzer_init(ctx);
+
+    char line[MAX_LINE_LEN];
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        size_t len = strlen(line);
+        while (len > 0 && (line[len - 1] == '\n' || line[len - 1] == '\r')) {
+            line[--len] = '\0';
+        }
+
+        if (len == 0) continue; 
+
+        LogRecord record;
+        if (parse_log_line(line, &record) == 0) {
+            analyzer_process_record(ctx, &record);
+        }
+    }
+
+    if (ferror(fp)) {
+        fprintf(stderr, "Error: Failed while reading the file.\n");
+    } else {
+        report_print(ctx);
+    }
+
+    free(ctx);
+    fclose(fp);
+
+    return EXIT_SUCCESS;
+}
